7464c6ef1863570440556929bebc9b0b707791a5
sammous <moustachir.sami@gmail.com>
2015-08-02 15:49:00 +0100

    navviewcontroller added

---
+++ tubestar/NavViewController.swift
@@ -0,0 +1,14 @@
+//
+//  NavViewController.swift
+//  tubestar
+//
+//  Created by Sami MOUSTACHIR on 02/08/2015.
+//  Copyright (c) 2015 Myintranet. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NavViewController: UIViewController{
+    
+}
