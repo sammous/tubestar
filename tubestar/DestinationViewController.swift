0625c4a402f74fabb0e1ad9a81bd902cb166c8e4
sammous <moustachir.sami@gmail.com>
2015-09-16 16:08:30 +0100

    icons touristics places

---
+++ tubestar/DestinationViewController.swift
@@ -0,0 +1,35 @@
+
+//
+//  DestinationViewController.swift
+//  tubestar
+//
+//  Created by Sami MOUSTACHIR on 16/09/2015.
+//  Copyright (c) 2015 Myintranet. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class DestinationViewController: UIViewController{
+    
+    
+    override func viewDidLoad() {
+        
+    }
+
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+        
+    }
+
+
+}
